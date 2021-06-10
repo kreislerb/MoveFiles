f9a1b873ee740a70c588213cc39c9c4a12d5bfa2
Kreisler Brenner Mendes <kreisler_551@hotmail.com>
2021-06-10 11:57:55 -0300

    Inserido formulário principal

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MoveFiles
         {
             InitializeComponent();
         }
+
+        private void btnFolderOrigin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
